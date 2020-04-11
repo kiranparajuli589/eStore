dbae0b495cda8926ed90e3d4d639b7f52da54ff5
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-04-11 22:55:26 +0545

    Added testcases for users with unicode nepali names and email

--- accounts/tests.py
+++ accounts/tests.py
@@ -7,6 +7,10 @@ class UserTestCase(TestCase):
         User.objects.create_superuser(email='test@admin.co', password='admin')
         User.objects.create_staff_user(email='test@staff.co', password='staff')
         User.objects.create_user(email='test@user.co', password='test')
+        User.objects.create(
+            email='किरण@user.co', password='test',
+            f_name='किरण', l_name='पराजुली'
+        )
 
     def test_create_superuser(self):
         user = User.objects.get(email='test@admin.co')
@@ -38,3 +42,7 @@ class UserTestCase(TestCase):
         self.assertEqual(user.get_full_name(), None)
         self.assertEqual(user.upper_case_name(), None)
 
+    def test_unicode_named_user(self):
+        user = User.objects.get(email='किरण@user.co')
+        self.assertEqual(user.f_name, 'किरण')
+        self.assertEqual(user.l_name, 'पराजुली')
